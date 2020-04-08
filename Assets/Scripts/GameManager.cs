20a752ad8fd07480bb5dae4258256dcf320ae3fe
jmv <44001505+jmvillaescusa@users.noreply.github.com>
2020-04-08 19:05:25 -0400

    Able to switch between Start and play screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    public ScreenManager screenManager;
+    // public GameObject pauseMenu;
+    // public GameObject gameUI;
+    
+    // private GameObject player;
+    // private GameObject donkeyKong;
+
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        // player = GameObject.Find("Player")
+        // donkeyKong = GameObject.Find("DonkeyKong")
+        SetInstance();
+        screenManager.SetInstance();
+    }
+
+    void SetInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PauseGame()
+    {
+        if (!isPaused)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+            // pauseMenu.SetActive(true);
+        }
+    }
+
+    public void UnpauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+            // pauseMenu.SetActive(false);
+        }
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void ResetGame()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public static ScreenManager Instance;
+
+    private GameObject StartScreen;
+    private GameObject PlayScreen;
+    public enum Screens { START, PLAY };
+    public Screens currentScreen;
+    
+    public void SetInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        StartScreen = GameObject.Find("Start");
+
+        PlayScreen = GameObject.Find("Play");
+    }
+
+    public void Update()
+    {
+        switch (currentScreen)
+        {
+            case Screens.START:
+                PlayScreen.SetActive(false);
+                StartScreen.SetActive(true);
+                break;
+
+            case Screens.PLAY:
+                StartScreen.SetActive(false);
+                PlayScreen.SetActive(true);
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && currentScreen == Screens.START)
+        {
+            currentScreen = Screens.PLAY;
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) && currentScreen == Screens.PLAY)
+        {
+            currentScreen = Screens.START;
+        }
+    }
+}
